e8392c78e5c9d23a46a03400baa92863a51cae7a
WIN-2ULUTV73FC1\Admin <sergeii.kirik@gmail.com>
2022-07-19 23:21:19 +0300

    upd

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
   fullName: {type: String, required: true},
   email: {type: String, required: true, unique: true},
   passwordHash: {type: String, required: true},
-  avatarUrl: {type: String, default: 'http://localhost:5000/uploads/android.jpg'}
+  avatarUrl: {type: String, default: '/uploads/android.jpg'}
 }, {
   timestamps: true
 })
