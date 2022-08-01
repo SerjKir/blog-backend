c72563e82141ad23b904492d2ab864b9428e015e
WIN-2ULUTV73FC1\Admin <sergeii.kirik@gmail.com>
2022-08-01 21:46:18 +0300

    refactoring

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
   fullName: {type: String, required: true},
   email: {type: String, required: true, unique: true},
   passwordHash: {type: String, required: true},
-  avatarUrl: {type: String, default: '/uploads/android.jpg'},
+  avatarUrl: {type: String, default: '/uploads/default_user.jpg'},
   posts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}]
 }, {
   timestamps: true
